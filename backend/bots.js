799c405b30f07d84af472608120693a81d2350b3
dinkiebinkie <b@binkie.town>
2019-10-01 18:05:29 -0400

    adding bot stuff

--- backend/bots.js
+++ backend/bots.js
@@ -1,1 +1,14 @@
-module.exports = [];
+module.exports = [
+  {
+    bot_id: 0,
+    bot_name: "test bot",
+    description: "I am a test! YOLO",
+    tweet_beginnings: [],
+    bot_type: "twitter",
+    bot_url: "https://twitter.com/testbot", // where does the bot live
+    number_of_likes: 200, // how many tweets does the bot like a day
+    ml_notebook: "path/of/ML", // who's ur daddy botty boy
+    tweet_storage_length: 100, // how many tweets are requested if it's running low
+    timer: 12 // how long (hours) until next tweet? variation of 75-125%
+  }
+];
